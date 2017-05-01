e593590979419ac2b13d27d7f8fc183703b04d0a
DiyanaDimitrova <H_E_R_A_92@abv.bg>
2017-05-01 16:23:06 +0300

    removed devpendecies

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,10 +1,13 @@
 'use strict'
 
 // routing and constants
-angular.module('socialApp', ['ngRoute']).constant('SERVER_URL', 'http://localhost:1337').config(function ($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'views/posts.html',
-    controller: 'postController',
-    controllerAs: 'postController'
-  }).otherwise({redirectTo: '/'})
+angular.module('socialApp', ['ngRoute']).constant('SERVER_URL', 'http://localhost:1337')
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'postController',
+      controllerAs: 'postController'
+    })
+    .otherwise({redirectTo: '/'})
 })
